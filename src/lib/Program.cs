85c83afe540a4eaecc1612049477defc354b84b1
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-01-31 14:49:43 -0500

    repo: reorganize project folder hierarchy

---
+++ src/lib/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ApiClient.Marketstack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
---
+++ src/test/UnitTestSample.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace ApiClient.marketstack.Test
+{
+    public class UnitTestSample
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+    
+}
