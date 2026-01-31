a072d7f437a104df8aa3548aff8466329507b47e
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-01-31 17:56:21 -0500

    test(marketstack): fix up sample test methods

--- src/test/Globals.cs
+++ src/test/Globals.cs
@@ -1,7 +1,11 @@
-using Xunit;
+global using System;
+global using Xunit;
 
 namespace ApiClient.Marketstack.xUnitTests
 {
+    /// <summary>
+    /// Defines names of attributes applied to objects via TraitAttribute.
+    /// </summary>
     internal record TestAttributeNames
     {
         public string Category { get; } = default!;
--- src/test/MarketstackApiTest.cs
+++ src/test/MarketstackApiTest.cs
@@ -1,16 +1,61 @@
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
-using Xunit;
+ using Microsoft.Extensions.Configuration;
  
 namespace ApiClient.Marketstack.xUnitTests
 {
-    public class MarketstackTests
+    public class ConfigurationFixture : IDisposable
+    {
+        public ConfigurationFixture()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddUserSecrets<ConfigurationFixture>()
+                .Build();
+        }
+        
+        public IConfiguration Configuration { get; }
+
+        public void Dispose() => GC.SuppressFinalize(this);
+    }
+
+    [Trait(nameof(TestAttributeNames.Category), "Unit")]
+    public class MarketstackTestsUnit
     {
         [Fact]
-        [Trait(nameof(TestAttributeNames.Category), "Unit")]
+        public void AssertTrue_ReturnsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+
+    [Trait(nameof(TestAttributeNames.Category), "Integration")]
+    public class MarketstackTestsIntegration : IClassFixture<ConfigurationFixture>
+    {
+        ConfigurationFixture _fixture;
+
+        public MarketstackTestsIntegration(ConfigurationFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Configuration_ApiKey_IsDefined()
+        {
+            // Arrange
+            
+            // Act
+            var api_key = _fixture.Configuration["api_key"];
+            Debug.WriteLine($"Found api_key={api_key}");
+            
+            // Assert
+            Assert.False(string.IsNullOrEmpty(api_key));
+        }
+
+        [Fact]
         public async Task GetEodDataAsync_ReturnSuccessResponse()
         {
             Assert.Fail();
         }
     }
-    
 }
