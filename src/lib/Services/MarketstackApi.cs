e431a8f850c047335ef2ba63d567f88304b10028
Isaac Hoey <48498580+hoeyi@users.noreply.github.com>
2026-02-15 16:26:48 -0500

    build: update target framework to .NET 10 LTS (#2)

--- src/lib/Services/MarketstackApi.cs
+++ src/lib/Services/MarketstackApi.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using ApiClient.Marketstack.Services;
 using System.Collections.Generic;
 using System.Linq;
-// using Serilog;
 
 namespace ApiClient.Marketstack
 {
