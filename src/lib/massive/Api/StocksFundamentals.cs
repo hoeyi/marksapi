741255d96f94435337bf5ecb2998e102e490e098
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-06-06 16:00:00 -0400

    fix: change parameter for short volume to optional

--- src/lib/massive/Api/StocksFundamentals.cs
+++ src/lib/massive/Api/StocksFundamentals.cs
@@ -21,7 +21,7 @@ public partial class MassiveApi
         string ticker,
         DateTime fromDate,
         DateTime toDate,
-        Interval<float> shortVolumeRatio,
+        Interval<float>? shortVolumeRatio = null,
         int? limit = 10
     )
     {
@@ -31,12 +31,17 @@ public partial class MassiveApi
         var queryBuilder = GetQueryBuilder();
         queryBuilder.AddParameter("date.gte", $"{fromDate:yyyy-MM-dd}");
         queryBuilder.AddParameter("date.lte", $"{toDate:yyyy-MM-dd}");
-        queryBuilder.AddParameter(
-            shortVolumeRatio.OpenLeft ? "short_volume_ratio.gt" : "short_volume_ratio.gte",
-            $"{shortVolumeRatio.Start}");
-        queryBuilder.AddParameter(
-            shortVolumeRatio.OpenRight ? "short_volume_ratio.lt" : "short_volume_ratio.lte",
-            $"{shortVolumeRatio.End}");
+
+        if (shortVolumeRatio.HasValue)
+        {
+            queryBuilder.AddParameter(
+                shortVolumeRatio.Value.OpenLeft ? "short_volume_ratio.gt" : "short_volume_ratio.gte",
+                $"{shortVolumeRatio.Value.Start}");
+            queryBuilder.AddParameter(
+                shortVolumeRatio.Value.OpenRight ? "short_volume_ratio.lt" : "short_volume_ratio.lte",
+                $"{shortVolumeRatio.Value.End}");
+        }
+        
         queryBuilder.AddParameter("limit", $"{limit}");
         
         var response = await GetResponseAsync<ShortVolumeResponse>(
