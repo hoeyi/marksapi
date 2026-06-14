09de4fd68e036550687bf01b0799fd682a899335
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-06-14 16:46:17 -0400

    feat: add method to RateTime for awaiting interval reset

--- src/lib/common/Services/RateTimer.cs
+++ src/lib/common/Services/RateTimer.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 using System.Timers; 
 
 namespace ApiClient.Services;
@@ -9,7 +13,9 @@ namespace ApiClient.Services;
 public class RateTimer
 {
     private short _counter;
-    private readonly Timer _timer;
+    private DateTime? _lastReset;
+    private readonly object _lock = new();
+    private readonly System.Timers.Timer _timer;
 
     /// <summary>
     /// Constructs a new instance of <see cref="RateTimer"/>.
@@ -28,8 +34,9 @@ public class RateTimer
         ApiCallLimit = apiCallLimit;
         ApiCallInterval = apiCallInterval;
 
-        _timer = new Timer(interval: apiCallInterval * 1000);
+        _timer = new System.Timers.Timer(interval: apiCallInterval * 1000);
         _timer.Elapsed += OnTimerElapsed;
+        _timer.Enabled = true;
     }
 
     /// <summary>
@@ -43,19 +50,90 @@ public class RateTimer
     public int ApiCallInterval { get; private init; } 
 
     /// <summary>
+    /// Gets the count of calls in the current interval.
+    /// </summary>
+    public short CurrentIntervalCalls => Counter;
+
+    /// <summary>
     /// Gets the rate-limiting status of this limiter.
     /// </summary>
-    public bool RateLimited => _counter == ApiCallLimit;
-
+    public bool RateLimited => Counter == ApiCallLimit;
+ 
     /// <summary>
-    /// Increments the counter.
+    /// Gets the <see cref="TimeSpan"/> representing the difference between now 
+    /// and the <see cref="LastReset"/> timestamp.
     /// </summary>
-    public void IncrementCounter() => _counter++;
+    public TimeSpan? TimeToReset => LastReset?.AddSeconds(ApiCallInterval).Subtract(DateTime.UtcNow);
 
     /// <summary>
-    /// Resets <see cref="_counter"/> to zero.
+    /// Gets or sets the count of API calls in this interval.
+    /// </summary>
+    private short Counter
+    {
+        get { return _counter; }
+        set
+        {
+            lock (_lock)
+            {
+                if(_counter != value)
+                {
+                    _counter = value;
+                    _lastReset = DateTime.UtcNow;
+                }
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Gets the UTC timestamp for the last time the <see cref="Counter"/> value was changed.
+    /// </summary>
+    private DateTime? LastReset => _lastReset;
+
+    /// <summary>
+    /// Increments the <see cref="Counter"> property.
+    /// </summary>
+    public void IncrementCounter() => Counter++;
+
+    /// <summary>
+    /// Checks to see if the rate limit has been tripped and awaits until the next reset before 
+    /// returning.
+    /// </summary>
+    /// <param name="ct">A <see cref="CancellationToken"/> instance.</param>
+    /// <returns>An empty <see cref="Task"/>.</returns>
+    public async Task AwaitIntervalResetAsync(CancellationToken? ct = null)
+    {
+        ct?.ThrowIfCancellationRequested();
+
+        // Validate parameters.
+        if(!RateLimited)
+            return;
+        
+        if(TimeToReset is null)
+            throw new InvalidOperationException(
+                $"Expected non-null value for '{nameof(TimeToReset)}'.");
+
+        while(Counter > 0)
+        {
+            if(ct?.IsCancellationRequested ?? false)
+            {
+                // Clean up here.
+                ct?.ThrowIfCancellationRequested();
+            }
+            Thread.Sleep(
+                timeout: TimeToReset ?? 
+                         new TimeSpan(hours: 0, minutes: 0, seconds: 0));
+        }
+
+        return;
+    }
+    
+    /// <summary>
+    /// Resets <see cref="Counter"/> to zero.
     /// </summary>
     /// <param name="source"></param>
     /// <param name="e"></param>
-    private void OnTimerElapsed(object? source, ElapsedEventArgs e) => _counter = 0;
+    private void OnTimerElapsed(object? source, ElapsedEventArgs e)
+    {
+        Counter = 0;
+    }
 }
