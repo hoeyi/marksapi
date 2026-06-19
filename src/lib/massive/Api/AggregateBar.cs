1655096fced8239fc446660d39d2d2c39e7d67e1
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-06-19 15:20:00 -0400

    feat(massive): add method for multi-ticker AggregateBar query
    Related #4
    Releted #5

--- src/lib/massive/Api/AggregateBar.cs
+++ src/lib/massive/Api/AggregateBar.cs
@@ -1,6 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
+using ApiClient.Massive.Response;
 using ApiClient.Massive.Response.Stocks;
+using ApiClient.Services;
+using Microsoft.Extensions.Logging;
 
 namespace ApiClient.Massive;
 
@@ -18,15 +25,46 @@ public partial class MassiveApi
                                     market, ticker, multiplier, timeSpan, from, to, limit);
 
     /// <inheritdoc/>
-    public Task<AggregateBarResponse> GetAggregateBarResponseAsync(
+    public async Task<AggregateBarResponse> GetAggregateBarResponseAsync(
         Market market,
-        string[] ticker,
+        string[] tickers,
         int multiplier,
         BarTimespanEnum timeSpan,
         DateTime from,
         DateTime to,
         int limit = 100)
     {
-        throw new NotImplementedException();
+        List<AggregateBarResponse> responses = [];
+
+        if(_rateTimer is null)
+                throw new InvalidOperationException(
+                    $"{nameof(GetTickerOverviewResponseAsync)} requires instance of '{nameof(RateTimer)}.");
+
+        foreach(var ticker in tickers)
+        {
+            if (_rateTimer.RateLimited)
+            {
+                await _rateTimer.AwaitIntervalResetAsync(ct: null);
+            }
+            else
+            {
+                var response = await GetAggregateBarResponseAsync(market, ticker, multiplier, timeSpan, from, to, limit);
+                _rateTimer.IncrementCounter();
+
+                if(response is null)
+                    _logger?.LogWarning("Received empty resonse.");
+                else
+                    responses.Add(response);
+            }
+        }
+        
+        var compositeResponse = new AggregateBarResponse()
+        {
+            RequestId = string.Join(",", responses.Select(x => x.RequestId)),
+            Ticker = string.Join(",", responses.Select(x =>x.Ticker)),
+            Status = string.Join(",", responses.Select(x => x.Status)),
+            Results = responses.SelectMany(x => x.Results).ToList()
+        };
+        return compositeResponse;
     }
 }
