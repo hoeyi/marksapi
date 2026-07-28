1b29cc91fb1925bf37daa18d14e3c9ef85755eb8
hoeyi <48498580+hoeyi@users.noreply.github.com>
2026-07-28 19:57:47 -0400

    refactor(marksapi): add build date to --version option

--- src/cli/ProgramExtensions.cs
+++ src/cli/ProgramExtensions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console;
@@ -45,9 +47,24 @@ namespace Ichyd.Marksapi.Cli
                 .GetExecutingAssembly()
                 .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                 ?.InformationalVersion;
+
+            var appBuildDateStr = Assembly
+                .GetExecutingAssembly()
+                .GetCustomAttributes<AssemblyMetadataAttribute>()
+                .FirstOrDefault(x => x.Key == "BuildDate")
+                ?.Value;
+            bool hasBuildDate = DateTime.TryParseExact(
+                appBuildDateStr,
+                "yyyyMMddHHmmss",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out DateTime buildDate);
+                
             var runtimeVersion = Environment.Version;
 
             Console.WriteLine($"App Version: {appInfoVersion}");
+            if(hasBuildDate)
+                Console.WriteLine($"Build date: {buildDate:yyyyMMdd-HHmmss}");
             Console.WriteLine($".NET Runtime Version: {runtimeVersion}");
             return 0;
         }
